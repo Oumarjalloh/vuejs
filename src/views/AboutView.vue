b08f523ae23263afde8ff8af278244f5e5acf345
Oumarjalloh <ojalloh38gmail.com>
2023-03-16 12:52:03 +0100

    oumar

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="about">
     <div v-for="homes in home" :key="homes.nom">
-    <h2>{{ homes.nom }}</h2>
-    <p>{{ homes.prix }} euro</p>
-    <p>{{ homes.stock }}</p>
+        <table class="tftable">
+<tr><th>Nom</th><th>Prix</th><th>Stock</th></tr>
+<tr>
+    <td>
+        <p>{{ homes.nom }}</p>
+    </td>
+    <td>
+        <p>{{ homes.prix }}€</p>
+    </td>
+    <td>
+        <p>{{ homes.stock }}</p>
+    </td>
+</tr>
+</table>
   </div>
   </div>
 </template>
@@ -39,3 +50,12 @@ export default {
  },
 }
 </script>
+<style>
+
+.tftable {font-size:12px;color:#333333;width:90%;border-width: 1px;border-color: #9dcc7a;border-collapse: collapse;}
+.tftable th {font-size:12px;background-color:#abd28e;border-width: 1px;padding: 5px;border-style: solid;border-color: #9dcc7a;text-align:left;}
+.tftable tr {background-color:#bedda7;}
+.tftable td {font-size:12px;border-width: 1px;padding: 8px;border-style: solid;border-color: #9dcc7a; text-align: left;}
+.tftable tr:hover {background-color:#ffffff;}
+
+</style>
